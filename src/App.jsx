80798ae1958c4fe9970af4da0b4086ecb8f4f40e
LucasPatricioRey <lucas1998z@hotmail.com>
2023-03-19 18:21:34 -0300

    preentrega1-rey

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import NavBar from "./components/NavBar";
+import Flex from "./components/Flex";
+
+function App() {
+  return (
+    <Flex>
+    <NavBar/>
+    </Flex>
+  );
+}
+
+export default App;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,16 @@
+import "./NavBar.css";
+
+
+function NavBar(){
+    return(
+        <nav >
+            <ul className="Nav-bar">
+                <li><a href="">Inicio</a></li>
+                <li><a href="">Productos</a></li>
+                <li><a href="">Contacto</a></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default NavBar;
